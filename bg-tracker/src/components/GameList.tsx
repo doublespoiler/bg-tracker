6575980a2137cc4ed91b0df020da7e9c0e7a6fda
Skylan Lew <skylanslew@gmail.com>
2022-12-15 17:27:39 -0700

    add cors-anywhere

--- bg-tracker/src/components/GameList.tsx
+++ bg-tracker/src/components/GameList.tsx
@@ -5,9 +5,31 @@ interface GameListProps{
 }
 
 const GameList:React.FC<GameListProps> = (props) => {
-  return(
-    <div className="game-list">
 
+  const[gameList, setGameList] = React.useState();
+
+  // var parseString = require('xml2js').parseString;
+  // var xml = fetch("https://api.geekdo.com/api/geekitem/linkeditems?linkdata_index=boardgame&objectid=2827&objecttype=property&showcount=25&sort=rank&subtype=boardgamemechanic").then(res => res.json());
+  // parseString(xml, function (err, result) {
+  //   console.dir(result);
+  // });
+  React.useEffect(() => {
+    var result = fetch("https://api.geekdo.com/api/geekitem/linkeditems?linkdata_index=boardgame&objectid=2827&objecttype=property&showcount=25&sort=rank&subtype=boardgamemechanic")
+      .then(res => res.json())
+      .then(data => setGameList(data.data.items));
+    console.log(result);
+  }, []);
+  
+  return(
+    // <div className="game-list">
+    //   {gameList.map((game) => 
+    //   <div>
+    //     {game.name}
+    //   </div>
+    //   )}
+    // </div>
+    <div>
+      <p>Game List</p>
     </div>
   )
 }
--- bg-tracker/src/components/MainContent.tsx
+++ bg-tracker/src/components/MainContent.tsx
@@ -14,8 +14,8 @@ const MainContent:React.FC<MainContentProps> = (props) => {
   return (
     <div>
       <GameList />
-      <GameDetail />
-      <SearchForm />
+      {/* <GameDetail />
+      <SearchForm /> */}
     </div>
   )
 }
