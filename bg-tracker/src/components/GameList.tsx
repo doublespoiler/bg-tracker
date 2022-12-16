2994312c168ccbf643f6540aed68dfe2179b8822
Skylan Lew <skylanslew@gmail.com>
2022-12-16 16:31:57 -0700

    get API call to work and render

--- bg-tracker/src/components/GameList.tsx
+++ bg-tracker/src/components/GameList.tsx
@@ -8,30 +8,39 @@ interface GameListProps{
 const GameList:React.FC<GameListProps> = (props) => {
 
 
-  const[gameList, setGameList] = React.useState(gamesData);
+  const[gameList, setGameList] = React.useState([]);
 
 
   React.useEffect(() => {
-
-
-    var result = fetch("https://crossorigin.me/https://api.geekdo.com/api/geekitem/linkeditems?linkdata_index=boardgame&objectid=1013&objecttype=property&showcount=25&sort=rank&subtype=boardgamecategory")
+    fetch(`https://api.allorigins.win/get?url=${encodeURIComponent('https://api.geekdo.com/api/geekitem/linkeditems?linkdata_index=boardgame&objectid=1013&objecttype=property&showcount=25&sort=rank&subtype=boardgamecategory')}`)
       .then(res => res.json())
-      .then(data => setGameList(data));
-      console.log(result);
-      console.log(gameList);
+      .then(data => {
+        console.log(data);
+        var values = Object.values(JSON.parse(data.contents))[0];
+        setGameList(values);
+      });
+
   }, []);
-  
+  console.log("game list " + typeof(gameList));
+  console.log(gameList);
+  //Object.keys returns just the names of the properties (items, itemdata, linkdata, config)
+  //Object.entries returns the properties and their values as key-value pairs
+  //Object.values returns only the values (what we want)
+  // const arr = Object.values(gameList)[0];
+  // console.log("arr ");
+  // console.log(arr);
+
   return(
-    // <div className="game-list">
-    //   {gameList.map((game) => 
-    //   <div>
-    //     {game.name}
-    //   </div>
-    //   )}
-    // </div>
-    <div>
-      <p>Game List</p>
+    <div className="game-list">
+      {gameList.map((game) => 
+      <div>
+        {game.name}
+      </div>
+      )}
     </div>
+    // <div>
+    //   <p>Game List</p>
+    // </div>
   )
 }
 
--- bg-tracker/src/components/MainContent.tsx
+++ bg-tracker/src/components/MainContent.tsx
@@ -11,6 +11,21 @@ interface MainContentProps{
 const MainContent:React.FC<MainContentProps> = (props) => {
   const [mainContent, setMainContent] = React.useState();
 
+  // const[gameList, setGameList] = React.useState(Object);
+
+
+  // React.useEffect(() => {
+  //   fetch(`https://api.allorigins.win/get?url=${encodeURIComponent('https://api.geekdo.com/api/geekitem/linkeditems?linkdata_index=boardgame&objectid=1013&objecttype=property&showcount=25&sort=rank&subtype=boardgamecategory')}`)
+  //     .then(res => res.json())
+  //     .then(data => {
+  //       console.log(data);
+  //       setGameList(Object.keys);
+
+  //     });
+
+  // }, []);
+  // console.log("game list" + gameList);
+
   return (
     <div>
       <GameList />
